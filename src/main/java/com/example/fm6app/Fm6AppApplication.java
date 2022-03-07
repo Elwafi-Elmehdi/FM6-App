586f42280114c54feafcc182cdeb4e556c55024c
Mehdi <elwafi.courrier99@gmail.com>
2022-03-07 09:24:33 +0100

    Initialized a spring project

---
+++ src/main/java/com/example/fm6app/Fm6AppApplication.java
@@ -0,0 +1,13 @@
+package com.example.fm6app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fm6AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fm6AppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fm6app/Fm6AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fm6app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fm6AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
