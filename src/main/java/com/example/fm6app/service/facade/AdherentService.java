291cac19d7fce3921a529753c24d7f84956f5608
Mehdi <elwafi.courrier99@gmail.com>
2022-03-07 10:22:23 +0100

    Added service layers interface and class for adherent

---
+++ src/main/java/com/example/fm6app/service/facade/AdherentService.java
@@ -0,0 +1,9 @@
+package com.example.fm6app.service.facade;
+
+import com.example.fm6app.domain.Adherent;
+
+import java.util.List;
+
+public interface AdherentService {
+    public List<Adherent> findAll();
+}
