8f31e4cde718e439ce9ac82aac27058f8a5dfc2f
Mehdi <elwafi.courrier99@gmail.com>
2022-03-28 14:24:47 +0000

    Added User Controller and implemented the SecurityCOnfig

--- src/main/java/com/example/fm6app/common/security/JWTAccessDenielHandler.java
+++ src/main/java/com/example/fm6app/common/security/JWTAccessDenielHandler.java
@@ -6,13 +6,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.OutputStream;
-
+@Component
 public class JWTAccessDenielHandler implements AccessDeniedHandler {
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
--- src/main/java/com/example/fm6app/config/WebSecurityConfig.java
+++ src/main/java/com/example/fm6app/config/WebSecurityConfig.java
@@ -1,4 +1,63 @@
 package com.example.fm6app.config;
 
-public class WebSecurityConfig {
+import com.example.fm6app.common.security.*;
+import com.example.fm6app.service.facade.UserService;
+import com.example.fm6app.service.implementation.UserServiceImplm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private JWTAuthorizationFilter jwtAuthorizationFilter;
+    @Autowired
+    private JWTAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    @Autowired
+    private JWTAccessDenielHandler jwtAccessDenielHandler;
+    @Autowired
+    private UserServiceImplm userServiceImp;
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .cors()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers(SecurityConsts.PUBLIC_URLS).permitAll()
+                .anyRequest().authenticated()
+                .antMatchers("/demandes/**","/criteres/**").hasAuthority("ROLE_ADMIN")
+                .antMatchers("/demandes/**").hasAuthority("ROLE_USER")
+                .and()
+                .exceptionHandling().accessDeniedHandler(jwtAccessDenielHandler)
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .and().addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userServiceImp).passwordEncoder(bCryptPasswordEncoder);
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception{
+        return super.authenticationManagerBean();
+    }
+    @Bean
+    BCryptPasswordEncoder getbCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
