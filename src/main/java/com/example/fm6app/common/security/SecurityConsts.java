3009db36cef4af46ce51ec755a0804c01ffb98eb
Mehdi <elwafi.courrier99@gmail.com>
2022-03-28 12:07:10 +0000

    Added user bean and security jwt commons

---
+++ src/main/java/com/example/fm6app/common/security/SecurityConsts.java
@@ -0,0 +1,19 @@
+package com.example.fm6app.common.security;
+
+import org.springframework.http.HttpStatus;
+
+public class SecurityConsts {
+    public static final long EXPIRATION_TIME = 432_000_000; // JWT 5 Days
+    public static final HttpStatus OK = HttpStatus.OK;
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String JWT_TOKEN_HEADER = "Jwt-Token";
+    public static final String TOKEN_CANNOT_BE_VERIFIED = "Token cannot be verified";
+    public static final String AUTHORITIES = "authorities";
+    public static final String FORBIDDEN_MESSAGE = "You need to log to access this page";
+    public static final String ACCESS_DENIED_MESSAGE = "You do not have permission to acess this page";
+    public static final String OPTION_HTTP_METHOD = "OPTION";
+    public static final String[] PUBLIC_URLS = {"/user/all/**","/user/login/**","/user/register/**","/user/resetpassword/**","/user/image/**"};
+    public static final String COMPANY_LLC = "Fondation Mohammed VI pour la Promotion des Oeuvres Sociales des Préposés religieux";
+    public static final String COMPANY_LLC_ADMINISTRATON = "Fondation Mohammed VI pour la Promotion des Oeuvres Sociales des Préposés religieux administration";
+    public static final String timeZone = "Morocco/Casablanca";
+}
